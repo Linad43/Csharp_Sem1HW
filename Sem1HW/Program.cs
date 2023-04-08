6b3d53b2c4e07d0ee138f2d429fa8e4c9051f424
Linad <merzlikindaniil97@gmail.com>
2023-04-08 17:17:25 +0300

    Первая задача решена

---
+++ Sem1HW/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Задача 2: Напишите программу, которая на вход принимает два числа
+ и выдаёт, какое число большее, а какое меньшее.*/
+
+Console.WriteLine("Програма определения большего числа");
+Console.Write("Ведите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a==b) Console.WriteLine("Числа равны (" + a + ")");
+else if (a>b) Console.WriteLine("Первое число (" + a + ") больше второго (" + b +")");
+else Console.WriteLine("Второе число (" + b + ") больше первого (" + a +")");
+
