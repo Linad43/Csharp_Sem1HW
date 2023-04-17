7f3b99c41c57ad432e647913ca61f2fa906eb89c
Linad <merzlikindaniil97@gmail.com>
2023-04-17 20:50:13 +0300

    all work

--- Sem3HW/Program.cs
+++ Sem3HW/Program.cs
@@ -6,103 +6,83 @@
 
 Console.WriteLine("Введите пятизначное число:");
 int num1 = Convert.ToInt32(Console.ReadLine());
-int i = 5;
-/*while (i>0)
+//int num1copy=num1;
+int N1 = 5;//N1=1
+int i = 1;
+bool ident=false;
+/*
+while (num1copy>=10)
 {
-    if (num1%(math.pow(10,i))
+    num1copy=num1copy/10;
+    N1++;
 }
 */
-
-Console.WriteLine($"{num1} -> {num1%Math.Pow(10,i-1)}");
-
-/*
-Задача 13: Напишите программу,
-которая выводит третью цифру заданного числа
-или сообщает, что третьей цифры нет.
-*/
-/*
-int rand = new Random().Next(0,2147483647);
-Console.WriteLine($"Случайное число: {rand}");
-int three=-1;
-
-if (rand >= 100 && rand < 1000)
+while (i<=(N1/2))
 {
-    three=rand%10;
+    if (Math.Floor((num1%Math.Pow(10,i))/(Math.Pow(10,i-1)))==Math.Floor((num1%Math.Pow(10,N1-i+1))/(Math.Pow(10,N1-i))))
+    {
+        ident = true;
+    }
+    else
+    {
+        ident = false;
+        break;
+    }
+    i++;
 }
-else if (rand >= 1000 && rand < 10000)
+if (ident)
 {
-    three=(rand/10)%10;
-}
-else if (rand >= 10000 && rand < 100000)
-{
-    three=(rand/100)%10;
-}
-else if (rand >= 100000 && rand < 1000000)
-{
-    three=(rand/1000)%10;
-}
-else if (rand >= 1000000 && rand < 10000000)
-{
-    three=(rand/10000)%10;
-}
-else if (rand >= 10000000 && rand < 100000000)
-{
-    three=(rand/100000)%10;
-}
-else if (rand >= 100000000 && rand < 1000000000)
-{
-    three=(rand/1000000)%10;
-}
-else if (rand >= 1000000000 && rand < 2147483647)
-{
-    three=(rand/10000000)%10;
-}
-if (three == -1)
-{
-    Console.WriteLine($"{rand} -> нет третьей цифры");
+    Console.WriteLine("Число палиндром");
 }
 else
 {
-    Console.WriteLine($"{rand} -> {three}");
+    Console.WriteLine("Число не палиндром");
 }
 
-// или проще следующим образом
-string sNum = rand.ToString();
-if (rand < 100)
-{
-    Console.WriteLine($"{rand} -> нет третьей цифры");
-}
-else 
-{
-    Console.WriteLine($"{rand} -> {sNum[2]}");
-}
-*/
+
+//Console.WriteLine($"{num1} -> {Math.Round((num1%Math.Pow(10,i))/(Math.Pow(10,i-1)),0)}");
+
 /*
-Задача 15: Напишите программу,
-которая принимает на вход цифру,
-обозначающую день недели, и проверяет,
-является ли этот день выходным.
+Напишите программу,
+которая принимает на вход координаты двух точек
+и находит расстояние между ними в 3D пространстве.
 */
+
+Console.WriteLine("Введите координаты первой точки:");
+Console.Write("A(x) = ");
+int Ax = Convert.ToInt32(Console.ReadLine());
+Console.Write("A(y) = ");
+int Ay = Convert.ToInt32(Console.ReadLine());
+Console.Write("A(z) = ");
+int Az = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки:");
+Console.Write("B(x) = ");
+int Bx = Convert.ToInt32(Console.ReadLine());
+Console.Write("B(y) = ");
+int By = Convert.ToInt32(Console.ReadLine());
+Console.Write("B(z) = ");
+int Bz = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введены точки:");
+Console.WriteLine($"A({Ax},{Ay},{Az})");
+Console.WriteLine($"B({Bx},{By},{Bz})");
+
+Console.Write("Расстояние между точками: ");
+Console.WriteLine(Math.Pow(Math.Pow(Ax-Bx,2)+Math.Pow(Ay-By,2)+Math.Pow(Az-Bz,2),0.5));
+
+
 /*
-Console.WriteLine ("Введите число дня недели");
-Console.WriteLine ("1. Понедельник");
-Console.WriteLine ("2. Вторник");
-Console.WriteLine ("3. Среда");
-Console.WriteLine ("4. Четверг");
-Console.WriteLine ("5. Пятница");
-Console.WriteLine ("6. Суббота");
-Console.WriteLine ("7. Воскресенье");
-int numWeek = Convert.ToInt32(Console.ReadLine());
-if (numWeek >= 1 && numWeek <=5)
-{
-    Console.WriteLine ("Это будний день");
-}
-else if (numWeek == 6 || numWeek == 7)
-{
-    Console.WriteLine ("Это выходной день");
-}
-else
-{
-    Console.WriteLine ("Не корректные данные");
-}
+Напишите программу,
+которая принимает на вход число (N)
+и выдаёт таблицу кубов чисел от 1 до N.
 */
+
+Console.WriteLine ("Введите число");
+int num3 = Convert.ToInt32(Console.ReadLine());
+int i1=2;
+Console.Write ($"{num3} -> 1");
+while (i1<=num3)
+{
+    Console.Write ($", {Math.Pow(i1,3)}");
+    i1++;
+}
+Console.Write ($".");
